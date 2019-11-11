2d0315e37a663f9e7d9e61f08a161b8f6e9d2eaf
NeoSon <lkloon123@hotmail.com>
2019-11-11 12:05:58 +0800

    (MoceanAPI/MIP#3) Add Recording API

--- Mocean/Voice/Voice.cs
+++ Mocean/Voice/Voice.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -32,6 +33,32 @@ namespace Mocean.Voice
             string responseStr = this.ApiRequest.Post("/voice/hangup/" + callUuid, this.parameters);
             return (HangupResponse)ResponseFactory.CreateObjectfromRawResponse<HangupResponse>(responseStr)
                 .SetRawResponse(this.ApiRequest.RawResponse);
+        }
+
+        public RecordingResponse Recording(string callUuid)
+        {
+            this.requiredFields = new List<string> { "mocean-api-key", "mocean-api-secret", "mocean-call-uuid" };
+
+            this.parameters["mocean-call-uuid"] = callUuid;
+            this.ValidatedAndParseFields();
+
+            string uri = "/voice/rec";
+
+            HttpResponseMessage response = this.ApiRequest.Send("GET", uri, this.parameters);
+
+            string contentHeader = response.Content.Headers.TryGetValues("Content-Type", out var value) ? value.FirstOrDefault() : null;
+            if ("audio/mpeg".Equals(contentHeader))
+            {
+                byte[] byteBody = response.Content.ReadAsByteArrayAsync().Result;
+                response.Dispose();
+
+                return new RecordingResponse(callUuid + ".mp3", byteBody);
+            }
+
+            //this method will throw exception if there's error
+            this.ApiRequest.FormatResponse(response.Content.ReadAsStringAsync().Result, response.StatusCode, this.parameters["mocean-resp-format"].Equals("xml", StringComparison.CurrentCultureIgnoreCase), uri);
+            response.Dispose();
+            return null;
         }
     }
 }
